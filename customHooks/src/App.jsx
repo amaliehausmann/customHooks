e931f8f552ca9752efac03e58701e5bb53af1d46
amaliehausmann <amaliehausmann@gmail.com>
2024-09-09 12:54:40 +0200

    image fix

--- customHooks/src/App.jsx
+++ customHooks/src/App.jsx
@@ -4,24 +4,22 @@ import { useImages } from "./hooks/useImages";
 
 function App() {
   const { data } = useFetch("https://jsonplaceholder.typicode.com/users");
-  console.log(data);
-
-  const images = useImages();
+  const { images }= useImages();
 
   return (
     <>
       <div>
-        {data.length > 0 &&
-          data?.map((item) => <p key={item.id}>{item.name}</p>)}
+        {data && data.length > 0 &&
+          data.map((item) => <p key={item.id}>{item.name}</p>)}
       </div>
       <div>
-        {images.length > 0 &&
+        {images && images.length > 0 &&
           images.map((image, index) => (
             <img
               className="imageStyling"
               key={index}
-              src={`public/${image}`}
-              alt=""
+              src={image}
+              alt=''
             />
           ))}
       </div>
--- customHooks/src/hooks/useImages.jsx
+++ customHooks/src/hooks/useImages.jsx
@@ -3,11 +3,26 @@ import { useEffect, useState } from "react";
 export function useImages() {
   const [images, setImages] = useState([]);
 
+  //   UseEffect der kun kører funktionen når siden loades
   useEffect(() => {
-    const imageArray = ["image1.jpg", "image2.jpg", "image3.jpg", "image4.jpg"];
+    // asynkron funktion der indlæser billeder fra public folder
+    async function loadImages() {
+      // Importerer alle billeder fra public mappen
+      const imageImports = import.meta.glob("../../public/*");
 
-    setImages(imageArray);
+      //   Henter alle stier til billederne fra imageImports objektet, hvor key er stien
+      const imagePaths = Object.keys(imageImports);
+
+      // Mapper stierne i et nyt array hvor '../../public/' fjernes med path.replace
+      const imageArray = imagePaths.map((path) => {
+        return path.replace("../../public/", "");
+      });
+
+      setImages(imageArray);
+    }
+
+    loadImages();
   }, []);
 
-  return images;
+  return { images };
 }
